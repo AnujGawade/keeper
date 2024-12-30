c1292ea070496b944a65b9371a2701ae0601b066
Anuj gawade <anujgawade1757@gmail.com>
2024-12-30 10:59:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import Footer from "../src/components/Footer/footer";
+import Header from "./components/Header/header";
+import Note from "../src/components/Note/note";
+import CreateArea from "../src/components/CreateArea/createArea";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+  return (
+    <div className="App">
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
